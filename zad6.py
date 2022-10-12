007d4aa36a9138055aff6c7240fdccd3a1670440
Michal Bienek <michal.bienek@edu.uekat.pl>
2022-10-12 21:06:35 +0200

    test

--- zad6.py
+++ zad6.py
@@ -6,6 +6,7 @@ import math
 def zad6(sample_list_1:list[int], sample_list_2:list[int])->list[int]:
     result_list=sample_list_1+sample_list_2
     result_list = list(dict.fromkeys(result_list))
+
     result_list =[x**3 for x in result_list]
     return result_list
 
