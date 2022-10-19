58b3c88764fddd10f6dfd745f403d52cf77543da
MichalUEKat <115018468+MichalUEKat@users.noreply.github.com>
2022-10-19 06:40:06 +0200

    Delete zad5.py

--- zad5.py
+++
@@ -1,8 +0,0 @@
-# Stworzyć funkcję, która przyjmie 2 argumenty. Pierwszy typu list , a drugi typu int
-# . Funkcja ma sprawdzić (zwracając typ logiczny bool ), czy lista z parametru
-# pierwszego zawiera taką wartość jaką przekazano w parametrze drugim.
-
-def contains_search_value(sample_list:list[int],search:int):
-    return search in sample_list
-
-print(contains_search_value([2,3,4,5],2))
