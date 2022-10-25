f6b1a9d542a15160c9350b46e6ad29ccf19a491d
Michal Bienek <michal.bienek@edu.uekat.pl>
2022-10-25 16:42:41 +0200

    format lab2

--- lab_02/zadanie2.py
+++ lab_02/zadanie2.py
@@ -1,36 +1,40 @@
-
-#a
-def print_names(names:list):
+# a
+def print_names(names: list):
     for name in names:
         print(name)
-print_names(["Ania","Wiktoria","Robert","Martyna","Wiktor"])
-#b
-def multiply_numbers_lc(numbers_list:list[int])->list[int]:
-    return [x*2 for x in numbers_list]
 
-def multiply_numbers_for(number_list:list[int])->list[int]:
+
+print_names(["Ania", "Wiktoria", "Robert", "Martyna", "Wiktor"])
+# b
+def multiply_numbers_lc(numbers_list: list[int]) -> list[int]:
+    return [x * 2 for x in numbers_list]
+
+
+def multiply_numbers_for(number_list: list[int]) -> list[int]:
     result = []
     for number in number_list:
-        result.append(number*2)
+        result.append(number * 2)
     return result
 
-print(multiply_numbers_lc([2,3,4,5,1]))
-print(multiply_numbers_for([2,3,4,5,1]))
 
-#c
+print(multiply_numbers_lc([2, 3, 4, 5, 1]))
+print(multiply_numbers_for([2, 3, 4, 5, 1]))
 
-def print_only_even(numbers:list[int]):
+# c
+
+
+def print_only_even(numbers: list[int]):
     for i in range(len(numbers)):
-        if(numbers[i]%2==0):
+        if numbers[i] % 2 == 0:
             print(numbers[i])
-        
 
-print_only_even([1,2,3,4,5,6,7,8,9,10])
 
-#d
-def print_every_other(number_list:list[int]):
-    result = [number_list[i] for i in range(len(number_list)) if i%2!=0 ]
+print_only_even([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# d
+def print_every_other(number_list: list[int]):
+    result = [number_list[i] for i in range(len(number_list)) if i % 2 != 0]
     print(result)
-print_every_other([1,2,3,4,5,6,7,8,9,10])
 
 
+print_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
